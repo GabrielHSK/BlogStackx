29ed4b146963688f75b253b771038be522515645
GabrielHSK <henrique.gabriel982@gmail.com>
2023-10-10 23:48:37 -0300

    mudando imagens

--- js/postpage.js
+++ js/postpage.js
@@ -22,7 +22,7 @@ const date_time_now = {
 switch (indice) {
   case "1":
     // <img src="" alt="imagem sugestiva" data_img></img>
-    html.data_img.setAttribute("src", "./img/NuHeu8ho.png_small");
+    html.data_img.setAttribute("src", "/BlogStackx/img/GIy-0yOi.png_small");
 
     // <span data_date>data</span>
     html.data_date.innerHTML = `${date_time_now.date} ${date_time_now.time}`;
@@ -31,14 +31,14 @@ switch (indice) {
     html.data_views.innerHTML = views_card1;
 
     // <h1 data_tittle>titulo</h1>
-    html.data_tittle.innerHTML = "Cameras e lentes";
+    html.data_tittle.innerHTML = "Cameras";
 
     // <p data_text>lorem</p>
-    html.data_text.innerHTML = "Publicação card1";
+    html.data_text.innerHTML = "Uma câmera fotográfica (português brasileiro) ou câmara fotográfica (português europeu) é um instrumento óptico para captação de imagens na forma de fotografias individuais, que são armazenadas localmente, transmitidas para outro local, ou ambos.Como capta informações sobre elementos externos sem ter contato físico com eles, tecnicamente é classificada como um dispositivo de sensoriamento remoto.";
 
 
     // <a href="" data_link></a>
-    html.data_link.setAttribute("href", "https://pt.wikipedia.org/wiki/IPhone_14")
+    html.data_link.setAttribute("href", "https://pt.wikipedia.org/wiki/C%C3%A2mera")
 
     break;
 
@@ -53,10 +53,10 @@ switch (indice) {
     html.data_views.innerHTML = views_card2;
 
     // <h1 data_tittle>titulo</h1>
-    html.data_tittle.innerHTML = "telescópio James Webb";
+    html.data_tittle.innerHTML = "Telescópio Espacial James Webb";
 
     // <p data_text>lorem</p>
-    html.data_text.innerHTML = "Publicação card2";
+    html.data_text.innerHTML = "A missão primária do JWST é a de examinar a radiação infravermelha resultante da grande expansão (Big Bang) e realizar observações sobre a infância do Universo. Para realizar tais estudos com uma sensibilidade sem precedentes, todo o Observatório é mantido frio, e as grandes fontes de interferência de infravermelho como o Sol, a Terra e a Lua são bloqueados.";
 
     html.data_link.setAttribute("href", "https://pt.wikipedia.org/wiki/Telesc%C3%B3pio_Espacial_James_Webb")
 
@@ -76,7 +76,7 @@ switch (indice) {
     html.data_tittle.innerHTML = "AI - Qual o limite?";
 
     // <p data_text>lorem</p>
-    html.data_text.innerHTML = "Publicação card3";
+    html.data_text.innerHTML = "Inteligência artificial (de sigla: IA; do inglês: Artificial Intelligence, de sigla: AI) é um campo de estudo multidisciplinar que abrange varias áreas do conhecimento.[1] Embora seu desenvolvimento tenha avançado mais na ciência da computação, sua abordagem interdisciplinar envolve contribuições de diversas disciplinas. ";
 
     html.data_link.setAttribute("href", "https://pt.wikipedia.org/wiki/Intelig%C3%AAncia_artificial")
 
@@ -85,7 +85,7 @@ switch (indice) {
 
   case "4":
     // <img src="" alt="imagem sugestiva" data_img></img>
-    html.data_img.setAttribute("src", "./img/Vntg3qeX.png_small");
+    html.data_img.setAttribute("src", "/BlogStackx/img/melhor-placa-de-v-deo-barata.webp");
 
     // <span data_date>data</span>
     html.data_date.innerHTML = `${date_time_now.date} ${date_time_now.time}`;
@@ -94,12 +94,12 @@ switch (indice) {
     html.data_views.innerHTML = views_card4;
 
     // <h1 data_tittle>titulo</h1>
-    html.data_tittle.innerHTML = "nova placa de video";
+    html.data_tittle.innerHTML = "Raytracing acelerado por hardware";
 
     // <p data_text>lorem</p>
-    html.data_text.innerHTML = "Publicação card4";
+    html.data_text.innerHTML = "A série GeForce 40 é uma família de unidades de processamento gráfico desenvolvida pela Nvidia, sucedendo a série GeForce 30. A série foi anunciada em 20 de setembro de 2022, no evento GPU Technology Conference (GTC) 2022; o RTX 4090 foi lançado em 12 de outubro de 2022, o RTX 4080 de 16 GB foi lançado em 16 de novembro de 2022 e o RTX 4070 Ti - originalmente anunciado como RTX 4080 de 12 GB - foi lançado em 5 de janeiro de 2023, com várias GPUs móveis a seguir em fevereiro de 2023. As placas são baseadas na arquitetura Ada Lovelace e apresentam raytracing acelerado por hardware (RTX) com os núcleos RT de terceira geração da Nvidia e os Tensor Cores de quarta geração.";
 
-    html.data_link.setAttribute("href", "https://pt.wikipedia.org/wiki/Placa_de_v%C3%ADdeo")
+    html.data_link.setAttribute("href", "https://pt.wikipedia.org/wiki/GeForce_40")
 
     break;
 
